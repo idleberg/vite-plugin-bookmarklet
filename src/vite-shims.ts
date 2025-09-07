e225b5985610f17ff5178bf0ec4783f0ec58d035
Jan T. Sott <git@idleberg.com>
2025-09-07 16:09:30 +0200

    chore: add jsdocs

--- src/vite-shims.ts
+++ src/vite-shims.ts
@@ -1,7 +1,21 @@
+/**
+ * This file contains utility functions adapted from Vite's source code.
+ */
+
+/**
+ * {@link {@see https://github.com/vitejs/vite/blob/v7.0.0/packages/vite/src/shared/utils.ts#L31}}
+ */
 const postfixRE = /[?#].*$/;
 
+/**
+ * Adapted from the raw query parameter
+ * {@link {@see https://github.com/vitejs/vite/blob/v7.0.0/packages/vite/src/node/utils.ts#L345}
+ */
 export const bookmarkletRE = /(\?|&)bookmarklet(?:&|$)/;
 
+/**
+ * {@link {@see https://github.com/vitejs/vite/blob/v7.0.0/packages/vite/src/shared/utils.ts#L32-L34}
+ */
 export function cleanUrl(url: string): string {
 	return url.replace(postfixRE, '');
 }
