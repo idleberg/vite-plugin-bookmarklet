d5566006979ccb4b477b9ea79e61853d4d9e0b7f
Jan T. Sott <git@idleberg.com>
2025-09-15 00:42:27 +0200

    chore: fix linter warnings

--- tests/unit/plugin.spec.ts
+++ tests/unit/plugin.spec.ts
@@ -3,6 +3,6 @@ import bookmarklet from '../hello-world.ts?bookmarklet';
 
 describe('bookmarklet', () => {
 	it('should contain alert with "Hello, World!"', () => {
-		expect(bookmarklet).toContain('javascript:(function(){alert(\'Hello%2C%20World!\')%3B%0A})()');
+		expect(bookmarklet).toContain("javascript:(function(){alert('Hello%2C%20World!')%3B%0A})()");
 	});
 });
